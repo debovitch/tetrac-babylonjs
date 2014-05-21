f5e040d4be61faaa97199e344616bc8507e9fe44
Thierry Duchassin <thierry.duchassin@gmail.com>
2014-05-21 18:34:54 +0200

    Added materials

--- js/TestScene.js
+++ js/TestScene.js
@@ -12,7 +12,7 @@ $.extend(TestScene.prototype, BABYLON.Scene.prototype);
 TestScene.prototype.createObjects = function() {
 
     // Adding the light to the scene
-    this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(-20, 50, 50), this);
+    this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(-20, 50, 0), this);
 
     // Adding an Arc Rotate Camera
     this.camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 100, new BABYLON.Vector3.Zero(), this);
@@ -25,6 +25,14 @@ TestScene.prototype.createObjects = function() {
 
     this.plane = BABYLON.Mesh.CreatePlane("Plane", 50.0, this);
     this.plane.position = new BABYLON.Vector3(4, -3, 20);
+
+    this.material1 = new BABYLON.StandardMaterial("texture1", this);
+    this.material1.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
+    this.sphere.material = this.material1;
+
+    this.material2 = new BABYLON.StandardMaterial("texture1", this);
+    this.material2.diffuseTexture = new BABYLON.Texture("img/country.jpg", this);
+    this.plane.material = this.material2;
 
     this.cylinder = BABYLON.Mesh.CreateCylinder("Cylinder", 3, 3, 3, 6, this, false);
     this.cylinder.position = new BABYLON.Vector3(0, -10, 2);
