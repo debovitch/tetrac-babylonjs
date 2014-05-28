96a9dd2595d7a8015b8c4d2a6c85b1778e067f01
Thierry Duchassin <thierry.duchassin@gmail.com>
2014-05-29 00:09:49 +0200

    hg

--- js/World.js
+++ js/World.js
@@ -25,29 +25,42 @@ function World() {
     this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(50, 50, 0), this.currentScene);
 
     // Adding an Arc Rotate Camera
-    this.camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3.Zero(), this.currentScene);
+    this.camera = new BABYLON.ArcRotateCamera("Camera", Math.PI/6, Math.PI/3, 50, new BABYLON.Vector3.Zero(), this.currentScene);
 
     this.currentScene.activeCamera.attachControl(this.canvas);
 
+    this.currentScene.clearColor = new BABYLON.Vector3(0.2, 0.2, 0.2);
+
     BABYLON.SceneLoader.ImportMesh(
-        "redCube", "/assets/", "redCube.babylon",
+        "", "/assets/", "tetrad.babylon",
         that.currentScene,
         function(newMeshes, particleSystems) {
 
             var yellowMat = new BABYLON.StandardMaterial("yellowMat", that.currentScene);
-            yellowMat.diffuseColor = new BABYLON.Color3(0.9451, 0.9216, 0.1137);
+            yellowMat.diffuseColor = new BABYLON.Color3(1.0000, 0.7000, 0.1000);
+
+            var board = BABYLON.Mesh.CreateBox("board", 2.5 * 5, that.currentScene);
+            var wireMaterial = new BABYLON.StandardMaterial("wire", that.currentScene);
+            wireMaterial.wireframe = true;
+            board.material = wireMaterial;
+            board.isVisible = false;
 
             for (var i=0; i<5; i++) {
                 for (var j=0; j<4; j++) {
                     for (var k=0; k<5; k++) {
-                        var yellowCube = newMeshes[0].clone("yellowCube" + i + j);
-                        yellowCube.position = new BABYLON.Vector3(4*i, 3*j, 4*k);
-                        if ((i+j+k)%2) {
-                            yellowCube.material = yellowMat;
+                        if (i+j+k) {
+                            var pawn = newMeshes[0].clone("pawn" + i + j + k);
+                            pawn.position = new BABYLON.Vector3(7*i, 3*j, 7*k);
+                            if ((i+j+k)%2) {
+                                pawn.material = yellowMat;
+                            }
+                            pawn.parent = board;
                         }
                     }
                 }
             }
+
+            board.locallyTranslate(new BABYLON.Vector3(-14, 0, -14));
 
             that.engine.runRenderLoop(
                 function() {
