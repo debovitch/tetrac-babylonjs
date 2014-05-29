dc179128583e9a16143668897a435b9fde071e20
Thierry Duchassin <thierry.duchassin@gmail.com>
2014-05-29 08:30:23 +0200

    Added lights and shadows

--- js/World.js
+++ js/World.js
@@ -22,7 +22,16 @@ function World() {
     this.currentScene = new BABYLON.Scene(this.engine);
 
     // Adding the light to the scene
-    this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(50, 50, 0), this.currentScene);
+    //this.omniLight1 = new BABYLON.PointLight("omniLight1", new BABYLON.Vector3(0, 20, 0), this.currentScene);
+    this.hemisphericLight1 = new BABYLON.HemisphericLight("hemisphericLight1", new BABYLON.Vector3(0, 1, 0), this.currentScene);
+    //this.spotLight1 = new BABYLON.SpotLight("spotLight1", new BABYLON.Vector3(0, 20, 0), new BABYLON.Vector3(0, -1, 0), Math.PI/2, 1, this.currentScene);
+    /*
+    this.directionalLight1 = new BABYLON.DirectionalLight("directionalLight1", new BABYLON.Vector3(-1, -1, 1), this.currentScene);
+    this.directionalLight1.position = new BABYLON.Vector3(20, 40, -20);
+
+    this.shadowGenerator = new BABYLON.ShadowGenerator(4096, this.directionalLight1);
+    this.shadowGenerator.useVarianceShadowMap = false;
+    */
 
     // Adding an Arc Rotate Camera
     this.camera = new BABYLON.ArcRotateCamera("Camera", Math.PI/6, Math.PI/3, 50, new BABYLON.Vector3.Zero(), this.currentScene);
@@ -30,6 +39,20 @@ function World() {
     this.currentScene.activeCamera.attachControl(this.canvas);
 
     this.currentScene.clearColor = new BABYLON.Vector3(0.2, 0.2, 0.2);
+
+    this.topPlate = BABYLON.Mesh.CreatePlane("topPlate", 40.0, this.currentScene);
+    this.topPlate.receiveShadows = true;
+    this.brownMaterial = new BABYLON.StandardMaterial("brownMat", that.currentScene);
+    this.brownMaterial.diffuseColor = new BABYLON.Color3(0.4730, 0.8170, 0.2710);
+    this.brownMaterial.
+    this.topPlate.material = this.brownMaterial;
+    this.topPlate.rotation.x = Math.PI/2;
+    this.topPlate.position.y = -2;
+
+    this.bottomPlate = this.topPlate.clone("bottomPlate");
+    this.bottomPlate.receiveShadows = false;
+    this.bottomPlate.rotation.x = -Math.PI/2;
+    this.bottomPlate.position.y = -2.1;
 
     BABYLON.SceneLoader.ImportMesh(
         "", "/assets/", "tetrad.babylon",
@@ -45,22 +68,25 @@ function World() {
             board.material = wireMaterial;
             board.isVisible = false;
 
+            newMeshes[0].parent = board;
+
             for (var i=0; i<5; i++) {
                 for (var j=0; j<4; j++) {
                     for (var k=0; k<5; k++) {
                         if (i+j+k) {
                             var pawn = newMeshes[0].clone("pawn" + i + j + k);
-                            pawn.position = new BABYLON.Vector3(7*i, 3*j, 7*k);
+                            pawn.position = new BABYLON.Vector3(7*i, 2.5*j, 7*k);
                             if ((i+j+k)%2) {
                                 pawn.material = yellowMat;
                             }
                             pawn.parent = board;
+                            //that.shadowGenerator.getShadowMap().renderList.push(pawn);
                         }
                     }
                 }
             }
 
-            board.locallyTranslate(new BABYLON.Vector3(-14, 0, -14));
+            board.locallyTranslate(new BABYLON.Vector3(-14, -0.7, -14));
 
             that.engine.runRenderLoop(
                 function() {
