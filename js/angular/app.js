2da24ca16dd9d9ce6ab06c888ddbe95d8d8b888e
Thierry Duchassin <thierry.duchassin@gmail.com>
2014-06-12 09:04:29 +0200

    Added angular-strap

--- js/angular/app.js
+++ js/angular/app.js
@@ -1,7 +1,12 @@
-angular.module('app', []);
+angular.module('app', ['mgcrea.ngStrap']);
 
 angular.module('app').controller('MainController', ['$scope', function($scope) {
 
     $scope.toto = "Toto";
+
+    $scope.aside = {
+        "title": "Title",
+        "content": "Hello Aside<br />This is a multiline message!"
+    };
 
 }]);
