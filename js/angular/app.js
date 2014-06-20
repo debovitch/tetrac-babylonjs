e2728607ca63c5ef74ed3e35ec85a53a2d22d452
Thierry Duchassin <thierry.duchassin@gmail.com>
2014-06-20 09:08:31 +0200

    Added bricks

--- js/angular/app.js
+++ js/angular/app.js
@@ -2,6 +2,13 @@ angular.module('app', []);
 
 angular.module('app').controller('MainController', ['$scope', function($scope) {
 
-    $scope.toto = "Toto";
+    $scope.bricks = [];
+    for (var i=0; i<25; i++) {
+        $scope.bricks.push(i);
+    }
+
+    for (var i=0; i<10; i++) {
+        $scope['active'+i] = true;
+    }
 
 }]);
